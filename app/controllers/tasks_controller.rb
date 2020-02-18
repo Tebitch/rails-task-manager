52026a379483798017e4e747188ac5f594a8fda2
Hiroki Kameyama <kamehiro1411@gmail.com>
2020-02-18 16:27:27 +0900

    new app has started to be created

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,45 @@
+class TasksController < ApplicationController
+
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(user_params)
+    @task.save
+
+    redirect_to tasks_path
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(user_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def user_params
+    params.require(:task).permit(:title, :details)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
+end
